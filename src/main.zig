43bdb20dc877df05f6966e4a411634af9f84eaf4
Alun Bestor <alun.bestor@shopify.com>
2020-09-18 22:52:00 -0400

    Remove redundant integration test for memlist parsing

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,5 @@ const meta = std.meta;
 test "Run all tests" {
     // Uncomment these if you have populated fixtures/dos with Another World game files.
     // meta.refAllDecls(@import("integration_tests/resource_loading.zig"));
-    // meta.refAllDecls(@import("integration_tests/memlist_parsing.zig"));
     meta.refAllDecls(@import("vm/instructions/instruction.zig"));
 }
