44ba3a3e3a38dac15445aab3ef65eae5412826bc
Alun Bestor <alun.bestor@shopify.com>
2020-09-20 14:11:26 -0400

    Add RegisterOr instruction

--- src/instructions/instruction.zig
+++ src/instructions/instruction.zig
@@ -22,6 +22,7 @@ const RegisterAdd = @import("register_add.zig");
 const RegisterAddConstant = @import("register_add_constant.zig");
 const RegisterAnd = @import("register_and.zig");
 const RegisterCopy = @import("register_copy.zig");
+const RegisterOr = @import("register_or.zig");
 const RegisterSet = @import("register_set.zig");
 const RegisterSubtract = @import("register_subtract.zig");
 const Return = @import("return.zig");
@@ -52,6 +53,7 @@ pub const Error =
     RegisterAddConstant.Error ||
     RegisterAnd.Error ||
     RegisterCopy.Error ||
+    RegisterOr.Error ||
     RegisterSet.Error ||
     RegisterSubtract.Error ||
     Return.Error ||
@@ -90,6 +92,7 @@ pub const Wrapped = union(enum) {
     RegisterAddConstant: RegisterAddConstant.Instance,
     RegisterAnd: RegisterAnd.Instance,
     RegisterCopy: RegisterCopy.Instance,
+    RegisterOr: RegisterOr.Instance,
     RegisterSet: RegisterSet.Instance,
     RegisterSubtract: RegisterSubtract.Instance,
     Return: Return.Instance,
@@ -124,6 +127,7 @@ pub fn parseNextInstruction(program: *Program.Instance) Error!Wrapped {
         .RegisterAddConstant => wrap("RegisterAddConstant", RegisterAddConstant, raw_opcode, program),
         .RegisterAnd => wrap("RegisterAnd", RegisterAnd, raw_opcode, program),
         .RegisterCopy => wrap("RegisterCopy", RegisterCopy, raw_opcode, program),
+        .RegisterOr => wrap("RegisterOr", RegisterOr, raw_opcode, program),
         .RegisterSet => wrap("RegisterSet", RegisterSet, raw_opcode, program),
         .RegisterSubtract => wrap("RegisterSubtract", RegisterSubtract, raw_opcode, program),
         .Return => wrap("Return", Return, raw_opcode, program),
@@ -165,6 +169,7 @@ pub fn executeNextInstruction(program: *Program.Instance, machine: *Machine.Inst
         .RegisterAddConstant => execute(RegisterAddConstant, raw_opcode, program, machine),
         .RegisterAnd => execute(RegisterAnd, raw_opcode, program, machine),
         .RegisterCopy => execute(RegisterCopy, raw_opcode, program, machine),
+        .RegisterOr => execute(RegisterOr, raw_opcode, program, machine),
         .RegisterSet => execute(RegisterSet, raw_opcode, program, machine),
         .RegisterSubtract => execute(RegisterSubtract, raw_opcode, program, machine),
         .Return => execute(Return, raw_opcode, program, machine),
@@ -232,6 +237,7 @@ test "parseNextInstruction returns expected instruction type when given valid by
     expectWrappedType(.RegisterAddConstant, try expectParse(&RegisterAddConstant.BytecodeExamples.valid));
     expectWrappedType(.RegisterAnd, try expectParse(&RegisterAnd.BytecodeExamples.valid));
     expectWrappedType(.RegisterCopy, try expectParse(&RegisterCopy.BytecodeExamples.valid));
+    expectWrappedType(.RegisterOr, try expectParse(&RegisterOr.BytecodeExamples.valid));
     expectWrappedType(.RegisterSet, try expectParse(&RegisterSet.BytecodeExamples.valid));
     expectWrappedType(.RegisterSubtract, try expectParse(&RegisterSubtract.BytecodeExamples.valid));
     expectWrappedType(.Return, try expectParse(&Return.BytecodeExamples.valid));
--- src/instructions/register_or.zig
+++ src/instructions/register_or.zig
@@ -5,7 +5,7 @@ const RegisterID = @import("../values/register_id.zig");
 
 const RegisterMask = u16;
 
-/// Applies a bitwise-AND mask to the value in a register.
+/// Applies a bitwise-OR mask to the value in a register.
 pub const Instance = struct {
     /// The ID of the register to apply the mask to.
     destination: RegisterID.Raw,
@@ -16,7 +16,7 @@ pub const Instance = struct {
     pub fn execute(self: Instance, machine: *Machine.Instance) void {
         // Register values are signed 16-bit values, but must be treated as unsigned in order to mask them.
         const original_value = machine.registers[self.destination];
-        const masked_value = @bitCast(RegisterMask, original_value) & self.value;
+        const masked_value = @bitCast(RegisterMask, original_value) | self.value;
         machine.registers[self.destination] = @bitCast(Machine.RegisterValue, masked_value);
     }
 };
@@ -36,7 +36,7 @@ pub const Error = Program.Error;
 // -- Bytecode examples --
 
 pub const BytecodeExamples = struct {
-    const raw_opcode = @enumToInt(Opcode.Enum.RegisterAnd);
+    const raw_opcode = @enumToInt(Opcode.Enum.RegisterOr);
 
     /// Example bytecode that should produce a valid instruction.
     pub const valid = [4]u8{ raw_opcode, 16, 0b1100_0011, 0b1111_0000 };
@@ -58,7 +58,7 @@ test "parse parses valid bytecode and consumes 3 bytes" {
 test "execute masks destination register" {
     const original_value: RegisterMask  = 0b1010_0101_1010_0101;
     const mask: RegisterMask            = 0b1100_0011_1111_0000;
-    const expected_value: RegisterMask  = 0b1000_0001_1010_0000;
+    const expected_value: RegisterMask  = 0b1110_0111_1111_0101;
 
     const instruction = Instance{
         .destination = 16,
