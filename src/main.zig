8e9d2e82e09093a45f6b56b5c86c9f3d5466c114
Alun Bestor <alunbestor@guldgubbe.local>
2020-11-14 23:04:22 +0000

    Update refAllDecls reference for zig 0.7.0

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,10 @@
 const std = @import("std");
 const testing = std.testing;
-const meta = std.meta;
 
 test "Run all tests" {
-    meta.refAllDecls(@import("rendering/video_buffer.zig"));
-    meta.refAllDecls(@import("rendering/polygon.zig"));
-    meta.refAllDecls(@import("instructions/instruction.zig"));
-    meta.refAllDecls(@import("integration_tests/resource_loading.zig"));
-    meta.refAllDecls(@import("integration_tests/bytecode_parsing.zig"));
+    testing.refAllDecls(@import("rendering/video_buffer.zig"));
+    testing.refAllDecls(@import("rendering/polygon.zig"));
+    testing.refAllDecls(@import("instructions/instruction.zig"));
+    testing.refAllDecls(@import("integration_tests/resource_loading.zig"));
+    testing.refAllDecls(@import("integration_tests/bytecode_parsing.zig"));
 }
