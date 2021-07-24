11aba87a133bc8bd2d79fc9911f01763d2da4d9e
alunbestor <alun.bestor@gmail.com>
2021-07-24 15:40:59 -0400

    Add documentation and tests for thread run function
    I'm not convinced the run function belongs in Thread; we need the whole Machine instance in order to test the function, which implies it belongs in Machine instead. We can decide that once Machine is more fleshed out.

--- src/machine/thread.zig
+++ src/machine/thread.zig
@@ -1,6 +1,20 @@
+//! This file defines the state for a thread in the Another World virtual machine.
+//!
+//! Each thread maintains its own paused/running state and its own program counter.
+//! Every tic, the virtual machine runs each active thread in sequence from 0 to 63.
+//! Each thread resumes executing the currently loaded program starting from the thread's
+//! previous program counter, and will run until an instruction yields to the next thread
+//! or deactivates the current thread.
+//!
+//! Each thread operates on the same bank of global registers; threads are not executed concurrently,
+//! so race conditions are not a concern. Separate threads were probably used to to simulate
+//! different entities within the game (enemies, projectiles etc.) as well as overall housekeeping
+//! for the current screen of the game.
+
 const Address = @import("../values/address.zig");
 const Machine = @import("machine.zig");
-const execute_next_instruction = @import("instruction.zig");
+const Program = @import("program.zig");
+const executeNextInstruction = @import("../instructions/instruction.zig").executeNextInstruction;
 
 const ExecutionState = union(enum) {
     /// The thread is active and will continue execution from the specified address when it is next run.
@@ -24,10 +38,6 @@ const SuspendState = enum {
 const max_instructions_per_tic = 10_000;
 
 /// One of the program execution threads within the Another World virtual machine.
-/// A thread maintains its own paused/running state and its current program counter.
-/// Each tic, the virtual machine runs each active thread: the thread resumes executing the current program
-/// starting from the thread's previous program counter, and will run until the thread yields to the next
-/// thread or deactivates itself.
 pub const Instance = struct {
     // Theoretically, a thread can only be in three functional states:
     // 1. Running at program counter X
@@ -44,15 +54,18 @@ pub const Instance = struct {
     // States 3 and 4 have the same effect; but we cannot rule out that a program will suspend
     // an inactive thread, then start running the thread *but expect it to remain suspended*.
     // To allow that, we must track each variable independently.
+
     /// The active/inactive execution state of this thread during the current game tic.
     execution_state: ExecutionState = .inactive,
+
     /// The scheduled active/inactive execution state of this thread for the next game tic.
     /// If `null`, the current state will continue unchanged next tic.
     scheduled_execution_state: ?ExecutionState = null,
 
     /// The running/suspended state of this thread during the current game tic.
     suspend_state: SuspendState = .running,
-    /// The scheduled running/suspended state of this thread for the current game tic.
+
+    /// The scheduled running/suspended state of this thread for the next game tic.
     /// If `null`, the current state will continue unchanged next tic.
     scheduled_suspend_state: ?SuspendState = null,
 
@@ -80,7 +93,7 @@ pub const Instance = struct {
         self.scheduled_suspend_state = .suspended;
     }
 
-    /// Apply any excheduled changes to the thread's execution and suspend states.
+    /// Apply any scheduled changes to the thread's execution and suspend states.
     pub fn update(self: *Instance) void {
         if (self.scheduled_execution_state) |new_state| {
             self.execution_state = new_state;
@@ -95,13 +108,10 @@ pub const Instance = struct {
 
     /// Execute the machine's current program on this thread, running until the thread yields
     /// or deactivates, or an error occurs, or the execution limit is exceeded.
-    pub fn run(self: *Instance, machine: *Machine) Error!void {
-        // Skip the thread if it's currently paused.
-        if (self.suspend_state == .paused) {
-            return;
-        }
+    pub fn run(self: *Instance, machine: *Machine.Instance) !void {
+        if (self.suspend_state == .suspended) return;
 
-        // If this thread is active, move the shared program counter to the last address for this thread;
+        // If this thread is active, resume executing the program from the previous address for this thread;
         // Otherwise, skip the thread.
         switch (self.execution_state) {
             .active => |address| try machine.program.jump(address),
@@ -113,14 +123,18 @@ pub const Instance = struct {
 
         var instructions_remaining: usize = max_instructions_per_tic;
         while (instructions_remaining > 0) : (instructions_remaining -= 1) {
-            const action = try executeNextInstruction(machine.program, machine);
+            const action = try executeNextInstruction(&machine.program, machine);
             switch (action) {
                 .Continue => continue,
-                .Yield => {
-                    // Yielding with a non-empty stack would cause the return address
-                    // for the current function to be lost after the shared stack gets cleared.
-                    // Once the thread resumes executing the function next tic, any Return
+                .YieldToNextThread => {
+                    // Yielding with a non-empty stack (i.e. in the middle of a function)
+                    // would cause the return address for the current function to be lost
+                    // once the next thread clears the stack.
+                    // When this thread resumes executing the function next tic, any Return
                     // instruction within that function would then result in error.StackUnderflow.
+                    // We're treating this as a programmer error, but it's possible that that
+                    // original game's code contains functions that only yield and
+                    // never return. If so, I'll remove this safety check.
                     if (machine.stack.depth > 0) {
                         return error.InvalidYield;
                     }
@@ -129,7 +143,7 @@ pub const Instance = struct {
                     self.execution_state = .{ .active = machine.program.counter };
                     return;
                 },
-                .Deactivate => {
+                .DeactivateThread => {
                     self.execution_state = .inactive;
                     return;
                 },
@@ -154,6 +168,8 @@ pub const Error = error{
 // -- Tests --
 
 const testing = @import("../utils/testing.zig");
+
+// - Schedule tests -
 
 test "scheduleJump schedules activation with specified program counter for next tic" {
     var thread = Instance{};
@@ -232,4 +248,71 @@ test "update applies scheduled suspend state" {
     thread.update();
     try testing.expectEqual(.running, thread.suspend_state);
     try testing.expectEqual(null, thread.scheduled_suspend_state);
+}
+
+// - Run tests -
+
+const Opcode = @import("../values/opcode.zig");
+
+test "run stores program counter upon reaching yield instruction and does not continue executing" {
+    const bytecode = [_]u8{
+        @enumToInt(Opcode.Enum.RegisterSet), 1, 0x0B, 0xAD, // Offset 0: Set register 1 to 0x0BAD
+        @enumToInt(Opcode.Enum.Yield), // Offset 3: Yield to next thread
+        @enumToInt(Opcode.Enum.RegisterSet), 2, 0xF0, 0x0D, // Offset 5: Set register 2 to 0xF00D
+    };
+
+    var machine = Machine.new();
+    machine.program = Program.new(&bytecode);
+
+    try machine.threads[0].run(&machine);
+    // First register-set should have been executed
+    try testing.expectEqual(0x0BAD, machine.registers[1]);
+    // Second register-set should not have been executed
+    try testing.expectEqual(0, machine.registers[2]);
+    try testing.expectEqual(.{ .active = 5 }, machine.threads[0].execution_state);
+}
+
+test "run deactivates thread upon reaching deactivate instruction" {
+    const bytecode = [_]u8{
+        @enumToInt(Opcode.Enum.RegisterSet), 1, 0x0B, 0xAD, // Offset 0: Set register 1 to 0x0BAD
+        @enumToInt(Opcode.Enum.Kill), // Offset 3: Kill current thread
+        @enumToInt(Opcode.Enum.RegisterSet), 2, 0xF0, 0x0D, // Offset 5: Set register 2 to 0xF00D
+    };
+
+    var machine = Machine.new();
+    machine.program = Program.new(&bytecode);
+
+    try machine.threads[0].run(&machine);
+    // First register-set should have been executed
+    try testing.expectEqual(0x0BAD, machine.registers[1]);
+    // Second register-set should not have been executed
+    try testing.expectEqual(0, machine.registers[2]);
+    try testing.expectEqual(.inactive, machine.threads[0].execution_state);
+}
+
+test "run returns error.InstructionLimitExceeded if program never yields or deactivates" {
+    const bytecode = [_]u8{
+        @enumToInt(Opcode.Enum.RegisterAddConstant), 1, 0, 2, // Offset 0: add 2 to register 1
+        @enumToInt(Opcode.Enum.Jump), 0x00, 0x00, // Offset 4: jump to offset 0 (infinite loop)
+    };
+
+    var machine = Machine.new();
+    machine.program = Program.new(&bytecode);
+
+    try testing.expectError(error.InstructionLimitExceeded, machine.threads[0].run(&machine));
+    try testing.expectEqual(max_instructions_per_tic, machine.registers[1]);
+}
+
+test "run returns error.InvalidYield if program yields in the middle of function" {
+    const bytecode = [_]u8{
+        @enumToInt(Opcode.Enum.Call), 0, 3, // Offset 0: call function at offset 3
+        @enumToInt(Opcode.Enum.RegisterSet), 1, 0x0B, 0xAD, // Offset 3: set register 1 to 0x0BAD
+        @enumToInt(Opcode.Enum.Yield), // Offset 7: yield to next thread
+    };
+
+    var machine = Machine.new();
+    machine.program = Program.new(&bytecode);
+
+    try testing.expectError(error.InvalidYield, machine.threads[0].run(&machine));
+    try testing.expectEqual(0x0BAD, machine.registers[1]);
 }
