970032b2a8af2aae2e627e3de03995089580e2e8
alunbestor <alun.bestor@gmail.com>
2022-01-04 20:07:12 -0500

    Replace separate input->register methods with registerValues struct
    The previous method-per-register implementation was conceptually elegant but did a lot of unnecessary work, repeatedly testing the same conditions.
    
    By combining them into a single method, we do all the work once; and the unit tests serve as a good visual model for how the various registers will change in sync with each other.

--- src/machine/user_input.zig
+++ src/machine/user_input.zig
@@ -29,99 +29,105 @@ pub const Instance = struct {
 
     // -- Machine register input values --
 
-    /// The value to insert into RegisterID.left_right_input.
-    pub fn leftRightInputRegisterValue(self: Self) Register.Signed {
-        // Reference implementation: left takes precedence over right
-        if (self.left) return -1;
-        if (self.right) return 1;
-        return 0;
-    }
+    /// Translates user input into values to insert into the appropriate registers,
+    /// for the game's bytecode to read the state of the input.
+    pub fn registerValues(self: Self) RegisterValues {
+        // All values will initially be 0.
+        var values = RegisterValues{};
 
-    /// The value to insert into RegisterID.up_down_input.
-    pub fn upDownInputRegisterValue(self: Self) Register.Signed {
-        // Reference implementation: up takes precedence over down
-        if (self.up) return -1;
-        if (self.down) return 1;
-        return 0;
-    }
-
-    /// The value to insert into RegisterID.action_input.
-    pub fn actionInputRegisterValue(self: Self) Register.Signed {
-        return if (self.action) 1 else 0;
-    }
-
-    /// The value to insert into RegisterID.movement_inputs.
-    pub fn movementInputsRegisterValue(self: Self) Register.BitPattern {
-        // zig fmt: off
-        var mask: u4          = 0b0000;
-        if (self.right) mask |= 0b0001;
-        if (self.left)  mask |= 0b0010;
-        if (self.down)  mask |= 0b0100;
-        if (self.up)    mask |= 0b1000;
-        // zig fmt: on
-
-        return mask;
-    }
-
-    /// The value to insert into RegisterID.all_inputs.
-    pub fn allInputsRegisterValue(self: Self) Register.BitPattern {
-        var mask = self.movementInputsRegisterValue();
-        if (self.action) mask |= 0b1000_0000; // 0x80
-        return mask;
-    }
-
-    /// The value to insert into RegisterID.last_character_typed.
-    /// Will be `nil` if the last character is unknown or not a supported character.
-    /// Should only be used during the password entry sequence.
-    pub fn lastCharacterTypedRegisterValue(self: Self) ?Register.Unsigned {
-        if (self.last_character_typed) |char| {
-            // TODO: It's not that we couldn't just dump *any* ASCII code into the register after lowercasing it,
-            // and let the bytecode just ignore unsupported values, but for now this matches the behaviour
-            // of the reference implementation (which was working with SDL keycodes rather than ASCII values):
-            // https://github.com/fabiensanglard/Another-World-Bytecode-Interpreter/blob/master/src/vm.cpp#L590-L596
-            return switch (char) {
-                'A'...'Z' => |uppercase_char| uppercase_char,
-                'a'...'z' => |lowercase_char| {
-                    // In ASCII, a-z are the same bit pattern as A-Z just with bit 6 set;
-                    // the reference implementation normalizes them to uppercase by unsetting that bit.
-                    const lowercase_bitmask: u8 = 0b0010_0000;
-                    const uppercase_char = lowercase_char & ~lowercase_bitmask;
-                    return uppercase_char;
-                },
-                // The SDL reference implementation permitted 8 (backspace, SDL's SDLK_BACKSPACE keycode).
-                '\x08' => 8,
-                // The SDL reference implementation permitted 0 (NUL, SDL's SDLK_UNKNOWN keycode),
-                // though it's unclear when that would ever be sent by the host.
-                '\x00' => 0,
-                // The SDL reference implementation commented out code that permitted 0xD
-                // (carriage return, SDL's SDLK_RETURN keycode).
-                // We could reenable it if the Another World bytecode actually responds to it.
-                '\r' => null,
-                // All other keys should not be sent.
-                else => null,
-            };
-        } else {
-            return null;
+        if (self.right) {
+            values.left_right_input = 1;
+            values.movement_inputs |= 0b0001;
         }
+
+        if (self.left) {
+            // Left movement takes precedence over right
+            values.left_right_input = -1;
+            values.movement_inputs |= 0b0010;
+        }
+
+        if (self.down) {
+            values.up_down_input = 1;
+            values.movement_inputs |= 0b0100;
+        }
+
+        if (self.up) {
+            // Up movement takes precedence over down
+            values.up_down_input = -1;
+            values.movement_inputs |= 0b1000;
+        }
+
+        values.all_inputs |= values.movement_inputs;
+
+        if (self.action) {
+            values.action_input = 1;
+            values.all_inputs |= 0b1000_0000;
+        }
+
+        if (self.last_character_typed) |char| {
+            values.last_character_typed = normalizedCharacter(char);
+        }
+
+        return values;
     }
 };
 
-// -- Fixture data --
+/// The register values corresponding to the current input state.
+pub const RegisterValues = struct {
+    /// The value to insert into `RegisterID.up_down_input`.
+    /// Will be -1 if left is active, 1 if right is active, 0 if neither is active.
+    up_down_input: Register.Signed = 0,
 
-const Fixtures = struct {
-    const no_inputs = Instance{};
-    const action_pressed = Instance{ .action = true };
-    const left = Instance{ .left = true };
-    const right = Instance{ .right = true };
-    const up = Instance{ .up = true };
-    const down = Instance{ .down = true };
-    const left_and_right = Instance{ .right = true, .left = true };
-    const up_and_down = Instance{ .up = true, .down = true };
-    const left_and_up = Instance{ .left = true, .up = true };
-    const right_and_down = Instance{ .right = true, .down = true };
-    const all_directions = Instance{ .left = true, .right = true, .up = true, .down = true };
-    const all_inputs = Instance{ .action = true, .left = true, .right = true, .up = true, .down = true };
+    /// The value to insert into `RegisterID.left_right_input`.
+    /// Will be -1 if left is active, 1 if right is active, 0 if neither is active.
+    left_right_input: Register.Signed = 0,
+
+    /// The value to insert into `RegisterID.action_input`.
+    /// Will be 0 if action is active, 0 otherwise.
+    action_input: Register.Signed = 0,
+
+    /// The value to insert into `RegisterID.movement_inputs`.
+    /// Bitflags of the currently active movement directions:
+    /// Bits 0, 1, 2, 3 correspond to right, left, down, up.
+    movement_inputs: Register.BitPattern = 0b0000,
+
+    /// The value to insert into `RegisterID.all_inputs`.
+    /// Contains bitflags of the currently active movement directions plus action:
+    /// Bits 0, 1, 2, 3 correspond to right, left, down, up, and bit 7 is the action flag.
+    all_inputs: Register.BitPattern = 0b0000_0000,
+
+    /// The value to insert into `RegisterID.last_character_typed`.
+    /// Contains the ASCII value of the most recently-pressed key,
+    /// or `null` if the key is unknown or does not correspond to a supported character.
+    last_character_typed: ?Register.Unsigned = null,
 };
+
+/// Given an ASCII character representing the most recently pressed key,
+/// normalizes it into a value supported by the Another World bytecode,
+/// or converts it to `null` if the character is unsupported.
+fn normalizedCharacter(char: u8) ?Register.Unsigned {
+    return switch (char) {
+        'A'...'Z' => |uppercase_char| uppercase_char,
+        'a'...'z' => |lowercase_char| {
+            // In ASCII, a-z are the same bit pattern as A-Z just with bit 6 set;
+            // the reference implementation normalizes them to uppercase by unsetting that bit.
+            const lowercase_bitmask: u8 = 0b0010_0000;
+            const uppercase_char = lowercase_char & ~lowercase_bitmask;
+            return uppercase_char;
+        },
+        // The SDL reference implementation permitted 8 (backspace, SDL's SDLK_BACKSPACE keycode).
+        '\x08' => 8,
+        // The SDL reference implementation permitted 0 (NUL, SDL's SDLK_UNKNOWN keycode),
+        // though it's unclear when that would ever be sent by the host.
+        '\x00' => 0,
+        // The SDL reference implementation commented out code that permitted 0xD
+        // (carriage return, SDL's SDLK_RETURN keycode).
+        // We could reenable it if the Another World bytecode actually responds to it.
+        '\r' => null,
+        // All other keys should not be sent.
+        else => null,
+    };
+}
 
 // -- Testing --
 
@@ -131,54 +137,153 @@ test "Ensure everything compiles" {
     testing.refAllDecls(Instance);
 }
 
-test "leftRightInputRegisterValue returns expected movement inputs" {
-    try testing.expectEqual(0, Fixtures.no_inputs.leftRightInputRegisterValue());
-    try testing.expectEqual(-1, Fixtures.left.leftRightInputRegisterValue());
-    try testing.expectEqual(1, Fixtures.right.leftRightInputRegisterValue());
-    // Should prioritise left input
-    try testing.expectEqual(-1, Fixtures.left_and_right.leftRightInputRegisterValue());
+// - registerValues tests -
+
+test "registerValues returns expected values on no input" {
+    const input = Instance{};
+    const expected = RegisterValues{};
+    try testing.expectEqual(expected, input.registerValues());
 }
 
-test "upDownInputRegisterValue returns expected movement inputs" {
-    try testing.expectEqual(0, Fixtures.no_inputs.upDownInputRegisterValue());
-    try testing.expectEqual(-1, Fixtures.up.upDownInputRegisterValue());
-    try testing.expectEqual(1, Fixtures.down.upDownInputRegisterValue());
-    // Should prioritise up input
-    try testing.expectEqual(-1, Fixtures.up_and_down.upDownInputRegisterValue());
+test "registerValues returns expected values on action input" {
+    const input = Instance{ .action = true };
+    const expected = RegisterValues{
+        .action_input = 1,
+        .all_inputs = 0b1000_0000,
+    };
+    try testing.expectEqual(expected, input.registerValues());
 }
 
-test "actionInputRegisterValue returns expected movement inputs" {
-    try testing.expectEqual(0, Fixtures.no_inputs.actionInputRegisterValue());
-    try testing.expectEqual(1, Fixtures.action_pressed.actionInputRegisterValue());
+test "registerValues returns expected values on key character input" {
+    const input = Instance{ .last_character_typed = 'a' };
+    const expected = RegisterValues{
+        .last_character_typed = 'A',
+    };
+    try testing.expectEqual(expected, input.registerValues());
 }
 
-test "movementInputsRegisterValue returns expected movement inputs" {
-    try testing.expectEqual(0b0000, Fixtures.no_inputs.movementInputsRegisterValue());
-    try testing.expectEqual(0b0001, Fixtures.right.movementInputsRegisterValue());
-    try testing.expectEqual(0b0010, Fixtures.left.movementInputsRegisterValue());
-    try testing.expectEqual(0b0100, Fixtures.down.movementInputsRegisterValue());
-    try testing.expectEqual(0b1000, Fixtures.up.movementInputsRegisterValue());
-    try testing.expectEqual(0b1010, Fixtures.left_and_up.movementInputsRegisterValue());
-    try testing.expectEqual(0b0101, Fixtures.right_and_down.movementInputsRegisterValue());
-    try testing.expectEqual(0b1111, Fixtures.all_directions.movementInputsRegisterValue());
-    try testing.expectEqual(0b1111, Fixtures.all_inputs.movementInputsRegisterValue());
+test "registerValues returns expected value on right input" {
+    const input = Instance{ .right = true };
+    const expected = RegisterValues{
+        .left_right_input = 1,
+        .movement_inputs = 0b0001,
+        .all_inputs = 0b0000_0001,
+    };
+    try testing.expectEqual(expected, input.registerValues());
 }
 
-test "allInputsRegisterValue combines expected movement inputs with action input" {
-    try testing.expectEqual(0b0000_1111, Fixtures.all_directions.allInputsRegisterValue());
-    try testing.expectEqual(0b1000_1111, Fixtures.all_inputs.allInputsRegisterValue());
+test "registerValues returns expected value on left input" {
+    const input = Instance{ .left = true };
+    const expected = RegisterValues{
+        .left_right_input = -1,
+        .movement_inputs = 0b0010,
+        .all_inputs = 0b0000_0010,
+    };
+    try testing.expectEqual(expected, input.registerValues());
 }
 
-test "lastCharacterTypedRegisterValue returns expected values" {
-    const all_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    for (all_uppercase) |char| {
-        const input = Instance{ .last_character_typed = char };
-
-        try testing.expectEqual(char, input.lastCharacterTypedRegisterValue());
-    }
+test "registerValues returns expected value on up input" {
+    const input = Instance{ .down = true };
+    const expected = RegisterValues{
+        .up_down_input = 1,
+        .movement_inputs = 0b0100,
+        .all_inputs = 0b0000_0100,
+    };
+    try testing.expectEqual(expected, input.registerValues());
 }
 
-test "lastCharacterTypedRegisterValue returns expected values for supported characters" {
+test "registerValues returns expected value on down input" {
+    const input = Instance{ .up = true };
+    const expected = RegisterValues{
+        .up_down_input = -1,
+        .movement_inputs = 0b1000,
+        .all_inputs = 0b0000_1000,
+    };
+    try testing.expectEqual(expected, input.registerValues());
+}
+
+test "registerValues returns expected value on left-and-right combined input" {
+    const input = Instance{ .left = true, .right = true };
+    const expected = RegisterValues{
+        // Left input should take precedence over right
+        .left_right_input = -1,
+        .movement_inputs = 0b0011,
+        .all_inputs = 0b0000_0011,
+    };
+    try testing.expectEqual(expected, input.registerValues());
+}
+
+test "registerValues returns expected value on up-and-down combined input" {
+    const input = Instance{ .up = true, .down = true };
+    const expected = RegisterValues{
+        // Up input should take precedence over down
+        .up_down_input = -1,
+        .movement_inputs = 0b1100,
+        .all_inputs = 0b0000_1100,
+    };
+    try testing.expectEqual(expected, input.registerValues());
+}
+
+test "registerValues returns expected value on up-and-left combined input" {
+    const input = Instance{ .up = true, .left = true };
+    const expected = RegisterValues{
+        .left_right_input = -1,
+        .up_down_input = -1,
+        .movement_inputs = 0b1010,
+        .all_inputs = 0b0000_1010,
+    };
+    try testing.expectEqual(expected, input.registerValues());
+}
+
+test "registerValues returns expected value on down-and-right combined input" {
+    const input = Instance{ .down = true, .right = true };
+    const expected = RegisterValues{
+        .left_right_input = 1,
+        .up_down_input = 1,
+        .movement_inputs = 0b0101,
+        .all_inputs = 0b0000_0101,
+    };
+    try testing.expectEqual(expected, input.registerValues());
+}
+
+test "registerValues returns expected value on all directions combined" {
+    const input = Instance{ .up = true, .down = true, .left = true, .right = true };
+    const expected = RegisterValues{
+        // Left should take precedence over right
+        .left_right_input = -1,
+        // Up should take precedence over right
+        .up_down_input = -1,
+        .movement_inputs = 0b1111,
+        .all_inputs = 0b0000_1111,
+    };
+    try testing.expectEqual(expected, input.registerValues());
+}
+
+test "registerValues returns expected value on all inputs combined" {
+    const input = Instance{
+        .up = true,
+        .down = true,
+        .left = true,
+        .right = true,
+        .action = true,
+        .last_character_typed = 'a',
+    };
+    const expected = RegisterValues{
+        // Left should take precedence over right
+        .left_right_input = -1,
+        // Up should take precedence over right
+        .up_down_input = -1,
+        .action_input = 1,
+        .movement_inputs = 0b1111,
+        .all_inputs = 0b1000_1111,
+        .last_character_typed = 'A',
+    };
+    try testing.expectEqual(expected, input.registerValues());
+}
+
+// - normalizedCharacter tests -
+
+test "normalizedCharacter returns expected values for supported characters" {
     const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const lowercase = "abcdefghijklmnopqrstuvwxyz";
     const backspace = '\x08';
@@ -186,49 +291,29 @@ test "lastCharacterTypedRegisterValue returns expected values for supported char
     const carriage_return = '\r';
 
     for (uppercase) |char| {
-        const input = Instance{ .last_character_typed = char };
-        try testing.expectEqual(char, input.lastCharacterTypedRegisterValue());
+        try testing.expectEqual(char, normalizedCharacter(char));
     }
 
     // lowercase characters should be converted to uppercase
     for (lowercase) |char, index| {
-        const input = Instance{ .last_character_typed = char };
-        try testing.expectEqual(uppercase[index], input.lastCharacterTypedRegisterValue());
+        try testing.expectEqual(uppercase[index], normalizedCharacter(char));
     }
 
-    {
-        const input = Instance{ .last_character_typed = backspace };
-        try testing.expectEqual(8, input.lastCharacterTypedRegisterValue());
-    }
-
-    {
-        const input = Instance{ .last_character_typed = NUL };
-        try testing.expectEqual(0, input.lastCharacterTypedRegisterValue());
-    }
-
-    {
-        const input = Instance{ .last_character_typed = carriage_return };
-        try testing.expectEqual(null, input.lastCharacterTypedRegisterValue());
-    }
+    try testing.expectEqual(8, normalizedCharacter(backspace));
+    try testing.expectEqual(0, normalizedCharacter(NUL));
+    try testing.expectEqual(null, normalizedCharacter(carriage_return));
 }
 
-test "lastCharacterTypedRegisterValue returns null when last character is not a supported character" {
+test "normalizedCharacter returns null when last character is not a supported character" {
     // These are not intended to be exhaustive 😁
     const numbers = "0123456789";
     const punctuation = ",./\\;:'\"<>{}()[]!@#$%^&*";
 
     for (numbers) |char| {
-        const input = Instance{ .last_character_typed = char };
-        try testing.expectEqual(null, input.lastCharacterTypedRegisterValue());
+        try testing.expectEqual(null, normalizedCharacter(char));
     }
 
     for (punctuation) |char| {
-        const input = Instance{ .last_character_typed = char };
-        try testing.expectEqual(null, input.lastCharacterTypedRegisterValue());
+        try testing.expectEqual(null, normalizedCharacter(char));
     }
-}
-
-test "lastCharacterTypedRegisterValue returns null when last character is unknown" {
-    const input = Instance{ .last_character_typed = null };
-    try testing.expectEqual(null, input.lastCharacterTypedRegisterValue());
 }
